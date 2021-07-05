65900bdf5b76608bfe1a0adec0f0fbf5f797c821
Puru Vijay <devpuruvj@gmail.com>
2021-07-06 01:48:18 +0530

    Fix compile script to not produce binaries

--- compile.sh
+++ compile.sh
@@ -2,7 +2,3 @@ env GOOS=windows GOARCH=amd64 go build -o bin/auto-class-launcher-windows.exe in
 env GOOS=darwin GOARCH=arm64 go build -o bin/auto-class-launcher-macos-intel index.go
 env GOOS=darwin GOARCH=amd64 go build -o bin/auto-class-launcher-macos-m1 index.go
 env GOOS=linux GOARCH=amd64 go build -o bin/auto-class-launcher-linux index.go
-
-chmod +x ./auto-class-launcher-macos-intel
-chmod +x ./auto-class-launcher-macos-m1
-chmod +x ./auto-class-launcher-macos-linux
